623f1bc7416c8c80f81b4b435f39978691b07f93
Fabricio Duarte <fabricio.duarte.jr@gmail.com>
2022-03-30 17:37:00 -0300

    Ajustar número das páginas baseado na ordem do SIGAA

--- src/main/java/com/imawa/sigaaforkotlin/models/Disciplina.kt
+++ src/main/java/com/imawa/sigaaforkotlin/models/Disciplina.kt
@@ -10,15 +10,15 @@ data class Disciplina(
 ) {
     companion object {
         const val PAGINA_PRINCIPAL = 0
-        const val PAGINA_PARTICIPANTES = 4
-        const val PAGINA_NOTICIAS = 7
-        const val PAGINA_FREQUENCIAS = 10
-        const val PAGINA_NOTAS = 6
-        const val PAGINA_CONTEUDOS = 9
-        const val PAGINA_REFERENCIAS = 8
-        const val PAGINA_ARQUIVOS = 5
-        const val PAGINA_AVALIACOES = 1
-        const val PAGINA_TAREFAS = 2
-        const val PAGINA_QUESTIONARIOS = 3
+        const val PAGINA_PARTICIPANTES = 3
+        const val PAGINA_NOTICIAS = 6
+        const val PAGINA_FREQUENCIAS = 7
+        const val PAGINA_NOTAS = 9
+        const val PAGINA_CONTEUDOS = 11
+        const val PAGINA_REFERENCIAS = 12
+        const val PAGINA_ARQUIVOS = 14
+        const val PAGINA_AVALIACOES = 15
+        const val PAGINA_TAREFAS = 16
+        const val PAGINA_QUESTIONARIOS = 17
     }
 }
